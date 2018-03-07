5d0758bf7e9e5f5c2c395056cae9385c63283bbf
Dan Zeitman <dvpweb@gmail.com>
2018-03-07 09:23:00 -0800

    Create sxsw-style-transfer.js

---
+++ sxsw-style-transfer.js
@@ -0,0 +1,37 @@
+/**
+* @param context {WebtaskContext}
+*/
+
+const cloudinary = require('cloudinary');
+
+
+module.exports = function(context, req ,res) {
+  
+  if(context.secrets.WT_API_KEY != context.data.auth_key){
+    const message = 'You are not authorized to use this api without an auth_key';
+    return  cb(null, message);
+  }
+  
+    // config cloudinary  
+   cloudinary.config({
+      "cloud_name": context.secrets.cloud_name,
+      "api_key": context.secrets.api_key,
+      "api_secret": context.secrets.api_secret
+    });
+    
+    const effect = context.data.effect ||  "style_transfer";
+    const public_id = context.data.pid ||  "domestic-dog_thumb_ceymam";
+    const overlay = context.data.overlay || "starrynight"; // "the-breeders-all-nerve";
+    
+    const strength =  context.data.strength ||  "";
+    var style_strength = "";
+    if(strength){
+      style_strength = ":preserve_color:" + strength || "";
+    }
+    
+    
+    var image = cloudinary.url(public_id + ".jpg",{overlay:overlay,effect: effect + style_strength, sign_url: true});
+    console.log(image)
+      res.writeHead(200, { 'Content-Type': 'text/html '});
+      res.end(image);
+};
