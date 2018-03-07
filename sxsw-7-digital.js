a6b2215fdd81ef05b91392cdb4a8ff177ffac746
Dan Zeitman <dvpweb@gmail.com>
2018-03-07 12:14:35 -0800

    Create sxsw-7-digital.js

---
+++ sxsw-7-digital.js
@@ -0,0 +1,196 @@
+const express    = require('express');
+const Webtask    = require('webtask-tools');
+const bodyParser = require('body-parser');
+const request = require('request');
+var app = express();
+
+var api, artists, consumerkey, consumersecret;
+
+app.use(bodyParser.json());
+
+// Our Middleware to setup API 
+var apiContext = function (req, res, next) {
+  const context = req.webtaskContext;
+  const page = context.data.page || 1;
+  const pageSize = context.data.pageSize || 100;
+  consumerkey = context.secrets.oauth_consumer_key;
+  consumersecret =  context.secrets.oauth_consumer_secret;
+  
+  api = require('7digital-api').configure({
+	  format: 'JSON',
+	  consumerkey: context.secrets.oauth_consumer_key,
+	  consumersecret: context.secrets.oauth_consumer_secret,
+	  defaultParams: { 
+	      country: 'GB', 
+	      usageTypes: 'adsupportedstreaming',  
+	      pageSize: pageSize, 
+	      page:page, 
+	      imageSize:800,
+	      sort: 'popularity desc'
+	  }
+});
+  artists = new api.Artists();
+  console.log('API Inited.')
+  next()
+}
+
+// Use our API Middleware
+app.use(apiContext)
+
+
+
+var getSong = function(context, trackid){
+  // For access to locker / subscription streaming without managed users you
+// will need to provide the accesstoken and secret for the user
+var oauth = new api.OAuth();
+    return new Promise(function (resolve, reject) {
+       var apiUrl = 'https://stream.svc.7digital.net/stream/catalogue?country=GB&trackid=' + trackid;
+       var signedURL = oauth.sign(apiUrl);
+       if(signedURL){
+          console.log(signedURL)
+          resolve(signedURL);
+       }else{
+          reject('we had an error');
+       }
+      });
+}
+
+app.get('/song/:trackid', function ( req, res) {
+  
+  const trackid = req.params.trackid  || '123456';  // /song/12345
+  const context = req.webtaskContext;
+
+  getSong(context, trackid).then(function(data){
+       // res.send( data);   
+        request(data).pipe(res);
+   }).catch(function(err){
+      console.log('ERR:', Err);
+      res.send(err);
+   })
+  
+});
+
+
+
+var getClip = function(trackid){
+  
+    return new Promise(function (resolve, reject) {
+      var clipUrl = 'http://previews.7digital.com/clip/' + trackid;
+      const oauth = new api.OAuth();
+      var previewUrl = oauth.sign(clipUrl);
+       if(previewUrl){
+          console.log(previewUrl)
+          resolve({ url:previewUrl });
+       }else{
+          reject('we had an error');
+       }
+      });
+}
+ 
+ app.get('/clip/:trackid', function ( req, res) {
+  var trackid = req.params.trackid || '12345';   // /clip/12345
+  getClip(trackid)
+  .then(function(data){
+        //res.send( data);   
+        request(data.url).pipe(res);
+   })
+   .catch(function(err){
+      console.log('ERR:', Err);
+      res.send(err);
+   })
+  
+});
+
+ 
+var browse = function(letter) {  
+  return new Promise(function (resolve, reject) {
+       artists.browse({ letter: letter }, function(err, data) {
+              if(err){
+               reject(err)
+              }
+              if(data){
+                resolve(data);
+              } 
+            });    
+  });
+}
+app.get('/browse/:letter', function ( req, res) {
+  const letter = req.params.letter;   // /browse/letter
+
+  browse(letter).then(function(data){
+        console.log(JSON.stringify(data,null,5));
+        res.send( data);   
+   }).catch(function(err){
+      console.log('ERR:', Err);
+      res.send(err);
+   })
+  
+});
+  
+  
+  var search = function(query) {  
+  return new Promise(function (resolve, reject) {
+        artists.search({ q: query }, function(err, data) {
+        if(err){
+          console.log(err);
+            reject(err)
+        }
+        if(data){
+          console.log(JSON.stringify(data,null,5));
+          resolve(data);
+        } 
+      });
+  })
+}
+  
+app.get('/search/:query', function ( req, res) {
+  
+  const context = req.webtaskContext;
+  const query = req.params.query || 1;
+ 
+  search(query).then(function(data){
+        res.send( data);   
+   })
+   .catch(function(error){
+      console.log('ERR: ', error);
+      res.send(error);
+   })
+  
+});
+  
+  
+  //14643 The Breeders
+
+var getReleases = function(artistID) {  
+  return new Promise(function (resolve, reject) {
+    
+        artists.getReleases({ artistid: artistID }, function(err, data) {
+        if(err){
+          console.log(err);
+            reject(err)
+        }
+        if(data){
+          console.log(data);
+          resolve(data);
+        } 
+      });
+  })
+}
+    
+app.get('/releases/:artistid', function ( req, res) {
+const artistid = req.params.artistid || '14643';
+  getReleases(artistid)
+  .then(function(data){
+        res.send( data);   
+   }).catch(function(err){
+      console.log('ERR:', Err);
+      res.send(err);
+   })
+});
+
+app.get('/', function (req, res) {
+  
+  res.sendStatus(200);
+});
+
+module.exports = Webtask.fromExpress(app);
