b635b93cb22c713c0e918ae6ac6043dadfbf1742
Dan Zeitman <dvpweb@gmail.com>
2018-03-08 14:12:16 -0800

    Updating sxsw-7-digital.js

--- sxsw-7-digital.js
+++ sxsw-7-digital.js
@@ -3,6 +3,7 @@ const express    = require('express');
 const Webtask    = require('webtask-tools');
 const bodyParser = require('body-parser');
 const request = require('request');
+var parsePath = require('parse-filepath');
 
 var app = express();
 
