0de9cffb99cdbf8397506461711dd4b03607df2a
Dan Zeitman <dvpweb@gmail.com>
2018-03-08 09:14:26 -0800

    Updating sxsw-7-digital.js

--- sxsw-7-digital.js
+++ sxsw-7-digital.js
@@ -23,7 +23,7 @@ var apiContext = function (req, res, next) {
 	  consumerkey: context.secrets.oauth_consumer_key,
 	  consumersecret: context.secrets.oauth_consumer_secret,
 	  defaultParams: { 
-	      country: 'US', 
+	     // country: 'GB', 
 	      shopId: 2020,
 	      usageTypes: 'adsupportedstreaming',  
 	      pageSize: pageSize, 
