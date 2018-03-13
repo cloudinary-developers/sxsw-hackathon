4b77a2b6837ae44a2535d5d797d89b6c96789397
Dan Zeitman <dvpweb@gmail.com>
2018-03-13 02:51:29 -0500

    Updating sxsw-7-digital.js

--- sxsw-7-digital.js
+++ sxsw-7-digital.js
@@ -104,7 +104,7 @@ var getLyrics = function(params){
   
 }
 
-app.get('/lyrics/:isrc', function (req, res) {
+app.get('/lyrics/?:isrc', function (req, res) {
   var track_isrc = req.params.isrc ||  'USCJ81000500'
   // var q_artist = req.params.artist  || 'Cyndi Lauper';  // /lyrics/The Breeders/
   // var q_track = req.params.track  || 'True Colors';  // /lyrics/The Breeders/All Nerve/
