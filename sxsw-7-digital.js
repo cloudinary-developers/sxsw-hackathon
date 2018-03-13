5dcb3a839bd91d663f2977d1693747bfd7bba59c
Dan Zeitman <dvpweb@gmail.com>
2018-03-13 02:47:46 -0500

    Updating sxsw-7-digital.js

--- sxsw-7-digital.js
+++ sxsw-7-digital.js
@@ -124,14 +124,14 @@ app.get('/lyrics', function (req, res) {
 
    getLyrics(data)
    .then(function(lyrics){
-     
+     // NLP reduce to key words
      Algorithmia.client(context.secrets.algorithmia_key)
     .algo("nlp/AutoTag/1.0.1")
     .pipe(lyrics.lyrics_body)
     .then(function(response) {
         console.log(response.get());
-        var r = {words:response.get(),lyrics: lyrics.lyrics_body };
-        res.send(r);
+        var results = {words:response.get(),lyrics: lyrics.lyrics_body };
+        res.send(results);
     });
     
           
