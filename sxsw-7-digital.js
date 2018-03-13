3bc1c29da3578b6bb7f51cc9b3fbae75b87de0a6
Dan Zeitman <dvpweb@gmail.com>
2018-03-13 02:38:03 -0500

    Updating sxsw-7-digital.js

--- sxsw-7-digital.js
+++ sxsw-7-digital.js
@@ -28,16 +28,13 @@ var apiContext = function (req, res, next) {
       "api_secret": context.secrets.api_secret
     });
 
-  
+  // paging 
   const page = context.data.page || 1;
   const pageSize = context.data.pageSize || 100;
   
+  //Music Match
   musicmatch_api_key = context.secrets.musicmatch_api_key;
-  
-  
-
-
-  
+  // 7digital-api
   consumerkey = context.secrets.oauth_consumer_key;
   consumersecret =  context.secrets.oauth_consumer_secret;
   
